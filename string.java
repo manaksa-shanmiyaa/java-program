9b98a32ab4bc925e3615d6b7a02e69c077dca3aa
manaksa-shanmiyaa <manumahivr@gmail.com>
2025-03-18 14:05:28 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,11 @@
+package day2;
+
+
+public class string {
+	public static void main(String[] args) {
+		String name="priya";
+				//String name= new String("priya");
+				System.out.println(name);
+	}
+
+}
